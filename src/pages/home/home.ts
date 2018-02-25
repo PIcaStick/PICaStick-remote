55808972f5bd3c672e247183bdcbc759d7f37b67
Théo Bougé <theo.bouge@gmail.com>
2018-02-25 15:47:05 +0100

    Add the dir-content component to the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-
-import {File} from '@ionic-native/file';
 import {NavController} from "ionic-angular";
 
 @Component({
@@ -8,19 +6,6 @@ import {NavController} from "ionic-angular";
     templateUrl: 'home.html'
 })
 export class HomePage {
-
-    dirContent: any;
-
-    constructor(private file: File,
-                public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
     }
-
-    ionViewWillEnter() {
-        this.file.listDir("file:///sdcard","DCIM").then((val) => {
-            console.log(JSON.stringify(val));
-        }).catch((reason) => {
-            console.error(JSON.stringify(reason));
-        })
-    }
-
 }
