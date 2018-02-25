6d37bbae4d86b1c3079a22a080c79bc7e7f97ca3
Théo Bougé <theo.bouge@gmail.com>
2018-02-25 17:34:19 +0100

    FORMAT file-system.ts

--- src/providers/file-system/file-system.ts
+++ src/providers/file-system/file-system.ts
@@ -10,7 +10,7 @@ export class FileSystemProvider {
 
     public getFileOrDirFromPath(path: string) {
         return this.file.listDir(
-            path.substring(0, path.lastIndexOf("/")+1),
+            path.substring(0, path.lastIndexOf("/") + 1),
             path.substring(path.lastIndexOf("/") + 1, path.length)
         ).then((directoryContent) => {
             return directoryContent;
@@ -18,5 +18,4 @@ export class FileSystemProvider {
             return Promise.reject(reason);
         })
     }
-
 }
