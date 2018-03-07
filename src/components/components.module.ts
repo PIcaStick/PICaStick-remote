ac31c58fbf60bdbf3fe69e680486264fea9ddd3f
TaKaQod <takaqod@gmail.com>
2018-03-07 18:21:54 +0100

    unnecesary modules

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DirContentComponent } from './dir-content/dir-content';
-@NgModule({
-	declarations: [DirContentComponent],
-	imports: [],
-	exports: [DirContentComponent]
-})
-export class ComponentsModule {}
